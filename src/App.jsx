1a7340f6a6d8002d34c569f5881a6117092903d9
firojhasan8080-debug <firojhasan8080@gmail.com>
2026-08-13 07:30:58 +0600

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div style={{ textAlign: 'center', padding: '50px', fontFamily: 'sans-serif' }}>
+      <h1 style={{ color: '#1e3a8a' }}>চিলমারী প্রাক-ক্যাডেট মাদ্রাসা</h1>
+      <p style={{ color: '#4b5563' }}>ওয়েবসাইট সফলভাবে Vercel-এ লাইভ হয়েছে!</p>
+    </div>
+  );
+}
